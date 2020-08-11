a777226bacc44ee6b93b6f5a8b02dcf373e9e0e4
SonNinh <sonninhngocba@gmail.com>
2020-08-11 21:08:36 +0700

    nothing

--- my_utils/save_to_txt.py
+++ my_utils/save_to_txt.py
@@ -1,36 +1,50 @@
+# -*- coding: utf-8 -*-
+
 import os
 from os import path
 import json
 import re
+from tqdm import tqdm
 
 
-def parse_json(content):
-    parten = re.compile(r'([^ \w])')
-    for para in content:
-        data = json.loads(para)['text']
-        # print(data)
-        data = re.split(' *(?:\n|\. |, )+ *', data)
-        for i in data:
-            print(parten.sub(r' \1 ', i))
-            print()
-        return 
-
-input_path = '/mnt/data/sonninh/vietnamese_tone/output/'
-output_path = '/mnt/data/sonninh/vietnamese_tone/all.txt'
-fw = open(output_path, 'w+')
-characters = '^[ _abcdefghijklmnopqrstuvwxyz0123456789áàảãạâấầẩẫậăắằẳẵặóòỏõọôốồổỗộơớờởỡợéèẻẽẹêếềểễệúùủũụưứừửữựíìỉĩịýỳỷỹỵđ!\"\',\-\.:;?_\(\)]+$'
-
-
-
-for root, dirs, files in os.walk(input_path):
-
-    for f in files:
+def write_in_the_star(files, parten1, parten2, parten3):
+    for f in tqdm(files):
         with open(path.join(root, f), 'r') as fr:
             content = fr.read()
-            content = re.sub("(\s)+", r"\1", content)
             content = content.split('\n')
-            parse_json(content)
-            
-            fr.close()
 
-        quit()
+            for para in content:
+                if para:
+                    data = json.loads(para)['text']
+                    data = parten3.split(data)
+                    for i in data:
+                        i_out = parten1.sub(r' \1 ', i)
+                        i_out = parten2.sub(r' ', i_out)
+                        yield i_out+'\n'
+        
+            fr.close()
+                
+
+if __name__ == "__main__":
+
+    input_path = '/home/sonninh/Downloads/transformer_with_self_attention/data'
+    output_path = '/home/sonninh/Downloads/transformer_with_self_attention/data_txt/{}.txt'
+    
+    characters = '^[ _abcdefghijklmnopqrstuvwxyz0123456789áàảãạâấầẩẫậăắằẳẵặóòỏõọôốồổỗộơớờởỡợéèẻẽẹêếềểễệúùủũụưứừửữựíìỉĩịýỳỷỹỵđ!\"\',\-\.:;?_\(\)]+$'
+
+    parten1 = re.compile(r'([^ \w])')
+    parten2 = re.compile(r'( {2,})')
+    parten3 = re.compile(r' *(?:\n|\. |, )+ *')
+
+    for root, dirs, files in os.walk(input_path):
+        if files:
+            file_name = path.basename(root)
+            fw = open(output_path.format(file_name), 'w+')
+            # write_in_the_star(files, parten1, parten2, parten3)
+            # for txt in write_in_the_star(files, parten1, parten2, parten3):
+            #     fw.write(txt)
+            fw.writelines(
+                write_in_the_star(files, parten1, parten2, parten3)
+            )
+            fw.close()
+            quit()
